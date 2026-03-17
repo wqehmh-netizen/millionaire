2df102146abf29d9dd4bc8cc0b9d69a6b431fc7b
wqehmh-netizen <wqehmh@gmail.com>
2026-03-18 02:22:30 +0300

    Create Footer.tsx

---
+++ app/app/components/components/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="px-6 md:px-10 py-8 mt-10 border-t border-white/10 text-center text-gray-400">
+      <p>Millionaire © 2025</p>
+      <p className="mt-2">طريقك للثراء يبدأ بخطوة</p>
+    </footer>
+  )
+}
